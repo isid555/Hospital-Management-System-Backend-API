2692a1aa58a07e475dcb8089e5f3bbba5d2bc5a5
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2025-03-08 13:07:43 +0300

    Add validation for user registration and login

---
+++ validation/validation.js
@@ -0,0 +1,26 @@
+const Joi = require('joi');
+
+// Register validation
+const registerValidation = (data) => {
+    const schema = Joi.object({
+        name: Joi.string().min(6).required(),
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required(),
+        role: Joi.string().valid('admin', 'doctor', 'nurse', 'patient').required()
+    });
+    return schema.validate(data, { abortEarly: false });
+};
+
+// Login validation
+const loginValidation = (data) => {
+    const schema = Joi.object({
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required()
+    });
+    return schema.validate(data, { abortEarly: false });
+};
+
+module.exports = {
+    registerValidation,
+    loginValidation
+}
