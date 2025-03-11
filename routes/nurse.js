2e6385e7822866b02d09cb76c0d6a09b9e35362f
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2025-03-11 16:11:18 +0300

    nurse route

---
+++ routes/nurse.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const nurseController = require('../controllers/nurseController');
+
+router.get('/', nurseController.getAllNurses); 
+router.get('/:id', nurseController.getNurseById);
+router.put('/:id', nurseController.updateNurse);
+router.delete('/:id', nurseController.deleteNurse)
+ 
+module.exports = router;
