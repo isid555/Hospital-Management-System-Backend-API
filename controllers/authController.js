ae16cca634b2d998e4d56405cccee660a19d30c2
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2025-03-12 15:23:25 +0300

    refactor authController to streamline user registration and login processes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,106 +1,138 @@
-const express = require('express');
-const User = require('../models/User');
-const Doctor = require('../models/Doctor');
-const Patient = require('../models/Patient');
-const Nurse = require('../models/Nurse');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { registerValidation, loginValidation } = require('../validation/validation');
-const data = require('../data/admin');
+const express = require("express");
+const User = require("../models/User");
+const Doctor = require("../models/Doctor");
+const Nurse = require("../models/Nurse");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const {
+  registerValidation,
+  loginValidation,
+} = require("../validation/validation");
 
 exports.register = async (req, res) => {
   // Validate the data before creating a user
   const { error } = registerValidation(req.body);
   if (error) {
-    const errorDetails = error.details.map(detail => detail.message);
+    const errorDetails = error.details.map((detail) => detail.message);
     return res.status(400).send(errorDetails);
   }
 
-  const role = req.body.role;
-  if (role === 'admin') return res.status(400).send('Admin cannot register here');
-
-  // Check if the user already exists
-  const emailExists = await User.findOne({ email: req.body.email });
-  if (emailExists) return res.status(400).send('Email already exists');
-
-  // Hash the password
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-  // Create a new user
-  const newUser = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: hashedPassword,
-    role: req.body.role
-  });
-
+  const { name, email, password, role, gender, phone } = req.body;
   try {
-    const savedUser = await newUser.save();
-
-    // Create a new role-specific document
-    let userRole;
-    if (role === 'doctor') {
-      userRole = new Doctor({
-        user: savedUser._id,
-        name: savedUser.name,
-        email: savedUser.email,
-        password: savedUser.password,
-        role: savedUser.role
-      });
-    } else if (role === 'patient') {
-      userRole = new Patient({
-        user: savedUser._id,
-        name: savedUser.name,
-        email: savedUser.email,
-        password: savedUser.password,
-        role: savedUser.role
-      });
-    } else if (role === 'nurse') {
-      userRole = new Nurse({
-        user: savedUser._id,
-        name: savedUser.name,
-        email: savedUser.email,
-        password: savedUser.password,
-        role: savedUser.role
-      });
-    } else {
-      return res.status(400).send('Invalid role');
+    let user = null;
+    if (role === "patient") {
+      user = await User.findOne({ email });
+    } else if (role === "doctor") {
+      user = await Doctor.findOne({ email });
+    } else if (role === "nurse") {
+      user = await Nurse.findOne({ email });
     }
 
-    await userRole.save();
+    // Check if user exists
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
 
-    res.status(200).json({
-      success: true,
-      message: "Successfully registered",
-      user: savedUser
-    });
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    if (role === "patient") {
+      user = new User({
+        name,
+        email,
+        password: hashPassword,
+        role,
+        gender,
+        phone,
+      });
+    }
+
+    if (role === "doctor") {
+      user = new Doctor({
+        name,
+        email,
+        password: hashPassword,
+        role,
+        gender,
+        phone,
+      });
+    }
+    if (role === "nurse") {
+      user = new Nurse({
+        name,
+        email,
+        password: hashPassword,
+        role,
+        gender,
+        phone,
+      });
+    }
+
+    await user.save();
+    res
+      .status(200)
+      .json({ success: true, message: "User successfully created" });
   } catch (err) {
-    res.status(400).send(err);
+    console.error("Error during registration:", err);
+    res
+      .status(500)
+      .json({ success: false, message: "Internal server error, Try again" });
   }
 };
 
-// Login Function
 exports.login = async (req, res) => {
-  // Validate the data before logging in
+  // Validate the data before login
   const { error } = loginValidation(req.body);
   if (error) {
-    const errorDetails = error.details.map(detail => detail.message);
+    const errorDetails = error.details.map((detail) => detail.message);
     return res.status(400).send(errorDetails);
   }
 
-  // Check if the email exists
-  const user = await User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send('Email or password is wrong');
+  const { email, password } = req.body;
+  try {
+    let user = null;
+    const patient = await User.findOne({ email });
+    const doctor = await Doctor.findOne({ email });
+    const nurse = await Nurse.findOne({ email });
 
-  // Check if the password is correct
-  const validPass = await bcrypt.compare(req.body.password, user.password);
-  if (!validPass) return res.status(400).send('Invalid password');
+    if (patient) {
+      user = patient;
+    }
+    if (doctor) {
+      user = doctor;
+    }
+    if (nurse) {
+      user = nurse;
+    }
 
-  // Create and assign a token
-  const token = jwt.sign({ _id: user._id, role: user.role }, process.env.SECRET_KEY);
-  res.header('auth-token', token).send({
-    message: "Successfully logged in",
-    token: token
-  });
+    // Check if the user exists
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+      return res
+        .status(400)
+        .json({ status: false, message: "Invalid Credentials" });
+    }
+
+    // Get token
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY, { expiresIn: '20d' });
+
+    const { password: userPassword, role, ...rest } = user._doc;
+
+    res.status(200).json({
+      status: true,
+      message: "Successfully Login",
+      token,
+      data: { ...rest },
+      role,
+    });
+  } catch (err) {
+    console.error("Error during login:", err);
+    res.status(500).json({ status: false, message: "Failed to login" });
+  }
 };
