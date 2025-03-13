90f381fd5256840560e2af7060d98e7fbbd55bb3
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2025-03-13 20:09:49 +0300

    verifyToken and verifyRole file

--- auth/verifytoken.js
+++ auth/verifytoken.js
@@ -1,25 +1,58 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const Doctor = require('../models/Doctor');
+const Nurse = require('../models/Nurse');
+const Admin = require('../models/Admin');
 
 function verifyToken(req, res, next) {
     const token = req.headers['auth-token'];
 
     if (!token) return res.status(401).send('No token provided. Access Denied');
     
-    try{
-        const verified = jwt.verify(token, process.env.SECRET_KEY);
-        req.user = verified;
+    try {
+        console.log('Token to Verify:', token); // Log the token
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        console.log('Verified Token:', decoded); // Log the decoded token
+        req.user = decoded;
         next();
-    }catch(err){
-        res.status(400).send('Invalid token');
+    } catch (err) {
+        console.error('Token Verification Error:', err); // Log the error
+        if (err.name === 'TokenExpiredError') {
+            return res.status(401).json({ message: 'Token is Expired' });
+        }
+
+        return res.status(401).json({ success: false, message: 'Invalid token' });
     }
 }
 
+const verifyRole = roles => async (req, res, next) => {
+    const userId = req.user._id;
 
-function verifyRole(role) {
-    return (req, res, next) => {
-        if (req.user.role !== role) return res.status(403).send('Access Denied');
-        next();
-    };
-}
+    let user;
+
+    const patient = await User.findById(userId);
+    const doctor = await Doctor.findById(userId);
+    const nurse = await Nurse.findById(userId);
+    const admin = await Admin.findById(userId);
+
+    if (patient) {
+        user = patient;
+    } else if (doctor) {
+        user = doctor;
+    } else if (nurse) {
+        user = nurse;
+    } else if (admin) {
+        user = admin;
+    }
+    console.log('User:', user); // Log the user
+    console.log('User Role:', user.role); // Log the user role
+    console.log('Required Role:', roles); // Log the roles
+
+    if (!user || !roles.includes(user.role)) {
+        return res.status(403).json({ success: false, message: "Access Denied" });
+    }
+
+    next();
+};
 
 module.exports = { verifyToken, verifyRole };
