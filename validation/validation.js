5780b2df01669b82f6fea0f21dbc48eafd1f7687
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2025-03-13 20:09:08 +0300

    Joi validation file

--- validation/validation.js
+++ validation/validation.js
@@ -6,7 +6,9 @@ const registerValidation = (data) => {
         name: Joi.string().min(6).required(),
         email: Joi.string().min(6).required().email(),
         password: Joi.string().min(6).required(),
-        role: Joi.string().valid('admin', 'doctor', 'nurse', 'patient').required()
+        role: Joi.string().valid('admin', 'doctor', 'nurse', 'patient').required(),
+        gender: Joi.string().valid('male', 'female').required(),
+        phone: Joi.number().required()
     });
     return schema.validate(data, { abortEarly: false });
 };
