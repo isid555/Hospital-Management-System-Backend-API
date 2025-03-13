2e5e3e6a2a37d51c173458979c28dbe375dc997d
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2025-03-13 20:04:49 +0300

    add appointment route

---
+++ routes/appointment.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const appointmentController = require('../controllers/appointmentController');
+const { verifyToken, verifyRole } = require('../auth/verifytoken');
+
+// Get all appointments for the authenticated user
+router.get('/my-appointments', verifyToken, verifyRole(['patient', 'doctor']), appointmentController.getUserAppointments);
+
+// Create a new appointment
+router.post('/', verifyToken, verifyRole(['patient']), appointmentController.createAppointment);
+
+// Get all appointments
+router.get('/', verifyToken, verifyRole(['admin']), appointmentController.getAllAppointments);
+
+// Get an appointment by ID
+router.get('/:id', verifyToken, verifyRole(['admin', 'doctor', 'patient']), appointmentController.getAppointmentById);
+
+// Update an appointment
+router.put('/:id', verifyToken, verifyRole(['doctor']), appointmentController.updateAppointment);
+
+// Delete an appointment
+router.delete('/:id', verifyToken, verifyRole(['doctor']), appointmentController.deleteAppointment);
+
+// Cancel an appointment
+router.put('/cancel/:id', verifyToken, verifyRole(['doctor', 'patient']), appointmentController.cancelAppointment);
+
+// Mark an appointment as completed
+router.put('/complete/:id', verifyToken, verifyRole(['doctor']), appointmentController.completeAppointment);
+
+module.exports = router;
